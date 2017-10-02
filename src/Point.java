bcaece0ae1532b01ff98d76291e727b08bca44e6
Vlad <pikl@ya.ru>
2017-10-02 23:46:03 +0900

    add new class

--- src/Point.java
+++ src/Point.java
@@ -2,4 +2,22 @@
  * Created by User on 02.10.2017.
  */
 public class Point {
+    private String figure_X;
+    private String figure_Y;
+
+    public String getFigureX (){
+        return figure_X;
+    }
+
+    public String getFigure_Y (){
+        return figure_Y;
+    }
+
+    public void setFigure_X (String x){
+        this.figure_X = x;
+    }
+
+    public void setFigure_Y (String y){
+        this.figure_Y = y;
+    }
 }
