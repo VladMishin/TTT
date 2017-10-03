b71a3f6e811c9fb80838734d9c6af6f4bb70feda
Vlad <pikl@ya.ru>
2017-10-03 20:51:32 +0900

    Add new class Figure

---
+++ src/Figure.java
@@ -0,0 +1,25 @@
+/**
+ * Created by User on 03.10.2017.
+ */
+public class Figure {
+    private String figureX;
+    private String figureY;
+
+    public String getFigureX (){
+        return figureX;
+    }
+
+    public String getFigureY (){
+            return figureY;
+        }
+
+    public void setFigureX (String x){
+            this.figureX = x;
+        }
+
+    public void setFigureY (String y){
+            this.figureY = y;
+        }
+    }
+
+}
