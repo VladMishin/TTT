fbfbb713168e8c23c33e400d068e8e7ce554acb8
Vlad <pikl@ya.ru>
2017-10-06 20:57:19 +0900

    Update class

--- src/main/java/Figure.java
+++ src/main/java/Figure.java
@@ -1,30 +1,6 @@
 package main.java;
 
-/**
- * Created by User on 03.10.2017.
- */
-public class Figure {
+public enum Figure {
 
-    private String figureX;
-
-    private String figureY;
-
-    public String getFigureX (){
-        return figureX;
-    }
-
-    public String getFigureY (){
-
-        return figureY;
-        }
-
-    public void setFigureX (String x){
-
-        this.figureX = x;
-    }
-
-    public void setFigureY (String y){
-
-        this.figureY = y;
-    }
+    X, O
 }
